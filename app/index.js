6fbb3a1c71036e29c203696581dca156f520538f
nick bouldien <nick.bouldien@gmail.com>
2017-05-31 22:44:03 -0700

    add in webpack.config.js and index.html to test functionality. changed scripts in package.json

--- app/index.js
+++ app/index.js
@@ -1,19 +1,37 @@
-import React, {Component} from 'react';
-import ReactDOM from 'reactDOM';
-import './index.css';
+// import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// // state
+// // lifecycle event
+// // UI
+//
+// class App extends Component {
+//   render(){
+//     return (
+//       <div>
+//         Hello World!
+//       </div>
+//     )
+//   }
+// }
+//
+// ReactDOM.render(<App />, document.getElementById('app'))
 
-// state
-// lifecycle event
-// UI
 
-class App extends Component {
-  render(){
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+class App extends React.Component {
+  render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <div>Hello World!</div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+
+
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
+
+
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+//
+// module.exports = {
+//   entry: './app/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'index_bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       { test: /\.(js)$/, use: 'babel-loader' },
+//       { test: /\.(css)$/, use: ['css-loader', 'style-loader'] }
+//     ]
+//   },
+//   plugins: [new HtmlWebpackPlugin({
+//     template: 'app/index.html'
+//   })]
+// }
