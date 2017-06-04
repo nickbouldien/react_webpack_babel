6c80eafc198f34f4b4bc641ba8322ed09728ecb3
nick bouldien <nick.bouldien@gmail.com>
2017-06-04 12:47:39 -0700

    add github api in utils, add RepoGrid to render the response from the api in Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 
 function SelectLanguage(props){
@@ -22,6 +23,128 @@ function SelectLanguage(props){
     </ul>
   )
 }
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
+
+// function RepoGrid(props) {
+//   console.log(props)
+//   return (
+//     <ul className="popular-list">
+//       {props.repos.map(function(repo,index) {
+return (
+//         <li key={repo.name} className='popular-item'>
+//           <div className='popular-rank'> #{index + 1}</div>
+//           <ul className='space-list-items'>
+//             <li>
+//               <img
+//                 className='avatar'
+//                 src={repo.owner.avatar_url}
+//                 alt={'avatar for ' + repo.owner.login}
+//               />
+//             </li>
+//             <li><a href={repo.html_url}>{repo.name}</a></li>
+//             <li>@{repo.owner.login}</li>
+//             <li>{repo.stargazer_count} stars</li>
+//           </ul>
+//         </li>
+//       })}
+//     </ul>
+//   )
+// }
+
+
+function RepoGrid (props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function (repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
+class Popular extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function(){
+      return {
+        selectedLanguage: lang,
+        repos: null
+      }
+    })
+
+    api.fetchPopularRepos(lang)
+      .then(function(repos) {
+        this.setState(function(){
+          return {
+            repos: repos
+          }
+        })
+      }.bind(this))
+  }
+
+  componentDidMount(){
+    this.updateLanguage(this.state.selectedLanguage)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+
+        {!this.state.repos
+          ? <p>loading...</p>
+          : <RepoGrid repos={this.state.repos} />}
+
+      </div>
+    )
+  }
+}
+
+export default Popular;
+
+// Badge.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   username: PropTypes.string.isRequired,
+//   img: PropTypes.string.isRequired
+// }
+
 
 
 // class SelectLanguage extends React.Component {
@@ -44,50 +167,4 @@ function SelectLanguage(props){
 //       </ul>
 //     )
 //   }
-// }
-
-SelectLanguage.propTypes = {
-  selectedLanguage: PropTypes.string.isRequired,
-  onSelect: PropTypes.func.isRequired
-}
-
-
-class Popular extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      selectedLanguage: 'All'
-    };
-
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
-
-  updateLanguage(lang) {
-    this.setState(function(){
-      return {
-        selectedLanguage: lang
-      }
-    })
-  }
-
-  render() {
-
-    return (
-      <div>
-        <SelectLanguage
-          selectedLanguage={this.state.selectedLanguage}
-          onSelect={this.updateLanguage}
-        />
-
-      </div>
-    )
-  }
-}
-
-export default Popular;
-
-// Badge.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   username: PropTypes.string.isRequired,
-//   img: PropTypes.string.isRequired
 // }
